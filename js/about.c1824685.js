aa2ca6353b92eb388c1492e4959759a1e80d97de
xiaosineng <xiaosineng@163.com>
2023-10-26 10:23:30 +0800

    update

---
+++ js/about.c1824685.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunktest"]=self["webpackChunktest"]||[]).push([[443],{9247:function(e,t,u){u.r(t),u.d(t,{default:function(){return p}});var a=function(){var e=this,t=e._self._c;return t("div",{staticClass:"about"},[t("p",[e._v("This is an "+e._s(e.page)+" page")])])},n=[],s={name:"AboutView",data(){return{page:null}},created(){this.page=this.$route.query.page},mounted(){}},r=s,l=u(1001),i=(0,l.Z)(r,a,n,!1,null,null,null),p=i.exports}}]);
+//# sourceMappingURL=about.c1824685.js.map
