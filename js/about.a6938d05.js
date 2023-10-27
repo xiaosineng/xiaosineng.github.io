c3169fdc80f102a6b890b36e0c5bdff3524e35da
xiaosineng <xiaosineng@163.com>
2023-10-27 16:16:39 +0800

    重定向

--- js/about.a6938d05.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunktest"]=self["webpackChunktest"]||[]).push([[443],{9820:function(e,t,u){u.r(t),u.d(t,{default:function(){return p}});var a=function(){var e=this,t=e._self._c;return t("div",{staticClass:"about"},[t("p",[e._v("This is an "+e._s(e.page)+" page")])])},n=[],s={name:"AboutView",data(){return{page:null}},created(){this.page=this.$route.query.page},mounted(){}},r=s,l=u(1001),i=(0,l.Z)(r,a,n,!1,null,null,null),p=i.exports}}]);
-//# sourceMappingURL=about.a6938d05.js.map
